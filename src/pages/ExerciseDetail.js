5457a829f54a0d826cbb6ddf641f270692aebf70
Hailley Kane <112648512+hailley-kane@users.noreply.github.com>
2023-03-02 09:28:23 -0600

    Add files via upload

---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {Box} from '@mui/material';
+
+const ExerciseDetail = () => {
+  return (
+    <div>
+      ExcerciseDetail
+    </div>
+  )
+}
+
+export default ExerciseDetail
