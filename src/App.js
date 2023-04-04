019862e86f94a9e2d24def6cfe9ba453481f5e87
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-04 11:24:22 -0500

    Update App.js
    imported the login page in the App.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Register from "./pages/Register";
-
+import Login from "./pages/Login";
 
 const App = () => {
   return (
