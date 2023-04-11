bfd25aeab74b72006b2eb3b966bc3720e67c3866
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-11 18:14:39 -0500

    Update Register.js
    once the user registers it would take them to the quiz page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -69,9 +69,9 @@ const REGISTER_URL = '/register';
             {success ?(
                     <section>
                         <h1>Success!</h1>
-                        <p>
-                            <a href="#">Sign In</a>
-                        </p>
+                        <button>
+                            <Link to="/Quiz">Take our Workout Quiz</Link>
+                        </button>
                     </section>
                 
                 ) : (
